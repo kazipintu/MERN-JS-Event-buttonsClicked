65e1aa19532a4b6346afd994b4d519310f4c5e54
kazipintu <kazipintu@gmail.com>
2024-05-04 04:33:42 +0530

    MERN/eShikhon/JS/Event/Practice-1

--- js/index.js
+++ js/index.js
@@ -4,13 +4,17 @@ function yellowColor() {
   document.body.style.backgroundColor = 'yellow';
 }
 
+
+
 /* Method-3 */
 const makeBlueButton = document.getElementById('make-blue')
-makeBlueButton.onclick = makeBlue();
+makeBlueButton.onclick = makeBlue(); // on page loading by default blue.
 
 function makeBlue() {
   document.body.style.backgroundColor = 'blue'
 }
+
+
 
 /* Method-4 */
 const makeRedButton = document.getElementById('make-red');
@@ -20,5 +24,29 @@ function makeRed() {
   //console.log('red button clicked');
   document.body.style.backgroundColor = 'red';
 }
+//console.log(makeRedButton)
 
-console.log(makeRedButton)
+
+
+/* Method-5 */
+const makeSkyBlueButton = document.getElementById('make-skyBlue');
+makeSkyBlueButton.onclick = function () {
+  document.body.style.backgroundColor = 'skyBlue';
+}
+
+
+
+/* Method-6 */
+const grayButton = document.getElementById('make-gray');
+//console.log(gray);
+grayButton.addEventListener('click', function(){
+  document.body.style.backgroundColor = 'gray';
+})
+
+
+
+/* Method-7 */
+document.getElementById ('make-black').addEventListener('click', function(){
+  //console.log('function clicked');
+  document.body.style.backgroundColor = 'black';
+})
